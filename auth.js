e9ccbafada94ed2f35fadf22c6f057f99fbf3de0
amykinney <amyelkinney@outlook.com>
2026-04-10 11:05:06 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,12 +1,8 @@
 function login() {
   const email = document.getElementById("email").value;
 
-  if (!email) {
-    alert("Please enter email");
-    return;
-  }
+  if (!email) return alert("Enter email");
 
   localStorage.setItem("user", email);
-
   window.location.href = "discover.html";
 }
